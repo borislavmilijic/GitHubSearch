b3577b3bbcc7742fe70694371dfbe1e6e99423df
Borislav <borislavmilijic@gmail.com>
2019-08-14 20:53:31 +0200

    Parser object - will be deleted probably

---
+++ Source/app/src/main/java/com/example/githubsearch/Parser.kt
@@ -0,0 +1,22 @@
+package com.example.githubsearch
+
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+
+
+object Parser {
+
+    /*
+
+    fun parseRepos (get_responce :String) :ArrayList<> {
+        val parser = JsonParser()
+        val result = null
+        val array = parser.parse(get_responce).asJsonArray
+
+
+
+    }
+    */
+}

c18aa89235db2079d8804e92cec06ca9d190382a
Borislav <borislavmilijic@gmail.com>
2019-08-14 20:51:56 +0200

    Added Recycler View, Searchbar and a custom row layout file

--- Source/app/src/main/java/com/example/githubsearch/MainActivity.kt
+++ Source/app/src/main/java/com/example/githubsearch/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.githubsearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
+
+    private val search_field: SearchView? = null
+    private val rec_view: RecyclerView? = null
+
+    val random_req = "https://api.github.com/search/repositories?q=tetris+in:readme,description,name"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.github.com/search/repositories?"
+        const val filter1 = ""
+        const val filter2 = ""
     }
 }
---
+++ Source/app/src/main/java/com/example/githubsearch/TestData.kt
@@ -0,0 +1,4 @@
+package com.example.githubsearch
+
+class TestData {
+}

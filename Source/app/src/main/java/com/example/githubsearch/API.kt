f4db016fe4670e6d4285cc0b2d4b649e462de463
Borislav <borislavmilijic@gmail.com>
2019-08-15 17:13:01 +0200

    Data Model & API Classes, app starts but no results showing

---
+++ Source/app/src/main/java/com/example/githubsearch/API.kt
@@ -0,0 +1,29 @@
+package com.example.githubsearch
+
+import retrofit2.http.GET
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Query
+
+interface API {
+
+    @GET("repositories?q=tetris+in:readme")
+    fun getReposObject(@Query("q") q: String): Observable<DataModel.Result>
+
+    companion object {
+        fun create(): API {
+
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(
+                    RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.github.com/search/")
+                .build()
+
+            return retrofit.create(API::class.java)
+        }
+    }
+}
+
--- Source/app/src/main/java/com/example/githubsearch/MainActivity.kt
+++ Source/app/src/main/java/com/example/githubsearch/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.githubsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
-import androidx.appcompat.widget.SearchView
-import android.widget.ListAdapter
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.TextView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
-    private val search_field: SearchView? = null
-    private val rec_view: RecyclerView? = null
+    var search_field: SearchView? = null
+    var tv_user: TextView? = null
+    val fetch_api by lazy { API.create() }
+    var disposable: Disposable? = null
 
     val random_req = "https://api.github.com/search/repositories?q=tetris+in:readme,description,name"
 
@@ -20,16 +21,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tv_user = findViewById(R.id.tv_users)
+        search_field = findViewById(R.id.search_bar_user)
 
-
-
+        getRepo()
     }
 
-    companion object {
-        const val BASE_URL = "https://api.github.com/search/repositories?"
-        const val filter1 = ""
-        const val filter2 = ""
+    fun showResult(result:String) {
+        tv_user?.setText(result)
     }
+
+    fun showError(e: String?) {
+        println(e)
+    }
+
+    private fun getRepo() {
+        disposable =
+            fetch_api.getReposObject("borislav")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { result -> showResult(result.items.name.repo_name) },
+                    { error -> showError(error.message) }
+                )
+    }
+
 }
 
 
